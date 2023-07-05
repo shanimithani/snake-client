cbcdb13ba0c1ee993f52d6f3c20c66dabb9bbdad
Shani Mithani <shanimithani@gmail.com>
2023-07-05 15:25:06 -0400

    Added name to snake

---
+++ client.js
@@ -0,0 +1,26 @@
+const net = require("net");
+
+// establishes a connection with the game server
+const connect = function () {
+  const conn = net.createConnection({
+    host: "165.227.47.243",
+    port: "50541"
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+
+  conn.on("data", (data) => {
+    console.log(data.toString());
+  });
+
+  conn.on("connect", () => {
+    conn.write("Name: SM7");
+  });
+
+  return conn;
+};
+
+module.exports = {
+  connect,
+};
